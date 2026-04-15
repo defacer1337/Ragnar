0beb0b1897704f73915db9325f136d7691284e95
Gabriel Amorim <bielamorim02@gmail.com>
2026-04-15 21:02:20 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 function Home() {
-    return <h1>Projeto/estudo Curso.Dev</h1>
+    return <h1>Estou registrando agora o 2° desafio, esse aqui é basicamente usando outros tipos de codigo, como: 
+        
+        -git status 
+        - git add pages 
+        - git status 
+        - git commit -m "modifica a frase" e por ultimo 
+        - git push.
+
+        Isso aqui esta muito bom, obrigado por me apresentar esta belezinha!! TAMO JUNTO @amazinho S2
+
+        OBS: Se deu certo.. Reage com um "S2" azul.
+    </h1>
 }
 
 export default Home; 
